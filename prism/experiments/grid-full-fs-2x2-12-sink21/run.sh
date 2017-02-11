21667089781c30570d445a2e7cca852ec91406ee
Sebastian Arming <sebastian@arming.net>
2017-02-11 21:35:28 +0100

    many experiments many wrong

--- prism/experiments/grid-full-fs-2x2-12-sink21/run.sh
+++ prism/experiments/grid-full-fs-2x2-12-sink21/run.sh
@@ -4,4 +4,4 @@ PARAMS="ne=0:1,nw=0:1,se=0:1,sw=0:1,en=0:1,es=0:1,wn=0:1,ws=0:1"
 CMD="$PRISM grid.prism Fgoal.props -param $PARAMS -javamaxmem 50g"
 
 NOW=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
-nohup $CMD 1>out.$NOW 2>err.$NOW &
+nohup $CMD 1>out.$NOW 2>err.$NOW 
