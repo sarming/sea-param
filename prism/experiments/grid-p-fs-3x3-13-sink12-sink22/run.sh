4e90f924f999a645c99b22902c1e0bf8f04d5f9d
Sebastian Arming <sebastian@arming.net>
2017-02-11 21:44:43 +0100

    fs-grid.prism -> grid.prism

--- prism/experiments/grid-p-fs-3x3-13-sink12-sink22/run.sh
+++ prism/experiments/grid-p-fs-3x3-13-sink12-sink22/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PRISM=../../bin/prism
 PARAMS="p=0:1"
-CMD="$PRISM fs-grid.prism Fgoal.props -param $PARAMS -javamaxmem 50g"
+CMD="$PRISM grid.prism Fgoal.props -param $PARAMS -javamaxmem 50g"
 
 NOW=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
 nohup $CMD 1>out.$NOW 2>err.$NOW 
